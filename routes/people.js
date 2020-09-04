3e015bd48f3d47f6cdcc8527901088e6293d25f5
Matt25969 <matt.jo.hunt@gmail.com>
2020-09-04 20:07:05 +0000

    Refactored

--- routes/people.js
+++ routes/people.js
@@ -2,12 +2,6 @@ const express = require("express");
 const router = express.Router();
 const axios = require('axios');
 
-// @route   GET people/peopleInLondon
-// @desc    Get all people that are either listed to live in London or live within 60 miles of London. 
-// @access  Public
-
-router.get("/peopleInLondon", (req, res) => {
-
 let coordEndPoint = "https://bpdts-test-app-v4.herokuapp.com/users"; 
 
 let cityEndPoint = "https://bpdts-test-app-v4.herokuapp.com/city/London/users";
@@ -16,26 +10,50 @@ const requestOne = axios.get(coordEndPoint);
 
 const requestTwo = axios.get(cityEndPoint);	
 
+// @route   GET people/peopleInLondon
+// @desc    Get all people that are either listed to live in London or live within 60 miles of London. 
+// @access  Public
+
+router.get("/peopleInLondon", (req, res) => {
+
 let result = [];
 
 axios.all([requestOne, requestTwo]).then(axios.spread((...responses) => {
 	
-	responses[0].data.forEach(function (item, index) {
-		  if (item.latitude >= 50 && item.latitude <= 52 && item.longitude >= -1 && item.longitude <= 1) {
-		  	result.push(item);
-		  }
-	})
+result.push(requestCallOne(responses[0]));
 
-	responses[1].data.forEach(function (item, index) {
-                result.push(item);
-        })
+result.push(requestCallTwo(responses[1]));
 
-	res.send(result);
+res.send(result);
 
   })).catch(error => {
 	  console.log(error);
   });
 
 });
+
+let requestCallOne = (input) => {
+
+let result = [];
+
+	input.data.forEach(function (item, index) {
+		if (item.latitude >= 50 && item.latitude <= 52 && item.longitude >= -1 && item.longitude <= 1) {
+			result.push(item);
+		}
+  })
+
+  return result;
+}
+
+let requestCallTwo = (input) => {
+
+	let result = [];
+
+	input.data.forEach(function (item, index) {
+		result.push(item);
+})
+
+	  return result;
+	}
 
 module.exports = router;
